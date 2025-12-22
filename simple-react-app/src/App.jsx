954826ce7f53fa73494901c9ed21b4f849009908
suryakanth2007 <suryajaldhani07@gmail.com>
2025-12-22 15:32:31 +0530

    Initial commit - Simple React App

---
+++ simple-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "60px" }}>
+      <h1>Hello React 👋</h1>
+      <p>This is a simple React application</p>
+    </div>
+  );
+}
+
+export default App;
+
