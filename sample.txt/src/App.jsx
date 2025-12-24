e4551a212730e4418a8794f518adcbb415fd4edb
suryakanth2007 <suryajaldhani07@gmail.com>
2025-12-24 09:47:17 +0530

    Initial React app

---
+++ sample.txt/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 My First React App</h1>
+      <p>Created using Vite + React</p>
+      <button
+        onClick={() => alert("Hello React!")}
+        style={{ padding: "10px 20px", fontSize: "16px" }}
+      >
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
